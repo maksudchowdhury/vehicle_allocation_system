74374e1cd0911f9271bde0372efac74e63c8380e
Maksud Chowdhury <chowdhurybox@gmail.com>
2024-10-28 09:15:02 +0600

    updating few files

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import sys; sys.dont_write_bytecode = True
 # ---------------------------------------------
 from fastapi import FastAPI
 from routes import employee, vehicle, allocation
-from utils.db import client
-from utils.db import employee_collection, vehicle_collection, allocation_collection
+
 
 app=FastAPI()
 
@@ -12,12 +11,3 @@ app=FastAPI()
 app.include_router(employee.router, prefix="/api")
 app.include_router(vehicle.router, prefix="/api")
 app.include_router(allocation.router, prefix="/api")
-
-
-# # POST endpoint to create an entry without a model
-# @app.post("/create")
-# async def create_entry(data: dict):
-#     # Insert the data directly into MongoDB
-#     result = await employee_collection.insert_one(data)
-    
-#     return {"inserted_id": str(result.inserted_id)}
--- utils/db.py
+++ utils/db.py
@@ -1,10 +1,9 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from motor.motor_asyncio import AsyncIOMotorClient
-# uri = "mongodb+srv://chowdhurycodes:wNbVymGHsnrfwaUG@vas.nrn80.mongodb.net/?retryWrites=true&w=majority"
+
 uri = "mongodb+srv://maksudchowdhury:mongo2024@fastapi-mongo.ucwg0.mongodb.net/?retryWrites=true&w=majority&appName=fastapi-mongo"
-# Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
+
 client = AsyncIOMotorClient(uri)
 db=client["vas_db"]
 employee_collection = db["employees_data"]
